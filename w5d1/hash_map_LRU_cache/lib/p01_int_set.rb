2bbdf716f3c6f28a6399185658061a27c4ca7ac3
jaronlee <jaronjlee@gmail.com>
2020-06-29 16:38:51 -0700

    added LRU_cach and hash_map classwork and CSS box

---
+++ w5d1/hash_map_LRU_cache/lib/p01_int_set.rb
@@ -0,0 +1,111 @@
+require "byebug"
+
+class MaxIntSet
+
+  attr_accessor :store
+  
+  def initialize(max)
+    @store = Array.new(max) {false}
+  end
+
+  def insert(num)
+    raise 'Out of bounds' if num > @store.length || num < 0
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[(num % @store.length)]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    if @count == @store.length 
+      resize!
+    end
+
+    if !self.include?(num)
+      self[num] << num 
+      @count += 1
+    end
+  end
+
+  def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    @count = 0
+    prev_store = @store.dup.flatten
+    @store = Array.new(num_buckets*2) {Array.new}
+    prev_store.each do |num|
+        self.insert(num)
+    end
+  end
+
+end

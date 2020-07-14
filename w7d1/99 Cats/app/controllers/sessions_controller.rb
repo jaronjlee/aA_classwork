99d2222bc993898c684cf454e794f5394019b07d
jaronlee <jaronjlee@gmail.com>
2020-07-13 17:01:20 -0700

    added cats_99

---
+++ w7d1/99 Cats/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    before_action :require_logged_out, only: [:new, :create]
+
+    def new
+        render :new 
+    end
+
+    def create
+        user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if user
+            login!(user)
+            redirect_to cats_url
+        else
+            render :new 
+        end
+    end    
+
+    
+
+
+end
